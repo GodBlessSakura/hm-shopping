ea643e49bee406dd02c8874dc4c3e8a3947c46d0
yangzhenghang <935878328@qq.com>
2023-10-08 14:03:01 +0800

    更新到点击登录跳转登录界面

--- src/api/login.js
+++ src/api/login.js
@@ -4,3 +4,24 @@ import request from '@/utils/request'
 export const getPicCode = () => {
   return request.get('/captcha/image')
 }
+
+export const getMessageCode = (captchaCode, captchaKey, mobile) => {
+  return request.post('/captcha/sendSmsCaptcha', {
+    form: {
+      captchaCode,
+      captchaKey,
+      mobile
+    }
+  })
+}
+
+export const condeLogin = (smsCode, mobile) => {
+  return request.post('/passport/login', {
+    form: {
+      smsCode,
+      mobile,
+      isParty: false,
+      partyData: {}
+    }
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,25 +9,27 @@
 
       <div class="form">
         <div class="form-item">
-          <input class="inp" maxlength="11" placeholder="请输入手机号码" type="text">
+          <input class="inp" v-model="mobile" maxlength="11" placeholder="请输入手机号码" type="text">
         </div>
         <div class="form-item">
-          <input class="inp" maxlength="5" placeholder="请输入图形验证码" type="text">
+          <input class="inp" v-model="picCode" maxlength="5" placeholder="请输入图形验证码" type="text">
           <img v-if="picUrl" :src="picUrl" @click="getPicCode" alt="">
         </div>
         <div class="form-item">
-          <input class="inp" placeholder="请输入短信验证码" type="text">
-          <button>获取验证码</button>
+          <input class="inp" v-model="smsCode" placeholder="请输入短信验证码" type="text">
+          <button @click="getCode">
+            {{ second === totalSecond ? '获取验证码' : second + '秒后重新发送' }}
+          </button>
         </div>
       </div>
 
-      <div class="login-btn">登录</div>
+      <div @click="login" class="login-btn">登录</div>
     </div>
   </div>
 </template>
 
 <script>
-import {getPicCode} from '@/api/login'
+import {condeLogin, getMessageCode, getPicCode} from '@/api/login'
 import {Toast} from 'vant'
 
 export default {
@@ -36,7 +38,12 @@ export default {
     return {
       picCode: '',
       pickey: '',
-      picUrl: ''
+      picUrl: '',
+      totalSecond: 60,
+      second: 60, // 当前秒数
+      timer: null, // 倒计时计时器
+      mobile: '',
+      smsCode: '' // 短信验证码
     }
   },
   async created() {
@@ -48,9 +55,56 @@ export default {
       const {data: {base64, key}} = await getPicCode()
       this.picUrl = base64
       this.pickey = key
+    },
+    // 校验手机号和图形验证码是否合法
+    validFn() {
+      if (!/^1[3-9]\d{9}$/.test(this.mobile)) {
+        this.$toast('请输入正确的手机号')
+        return false
+      }
+      if (!/^\w{4}$/.test(this.picCode)) {
+        Toast('请输入正确的图形验证码')
+        return false
+      }
+      return true
+    },
 
-      Toast('获取图形验证码成功')
+    async login() {
+      if (!this.validFn()) {
+        return
+      }
+
+      if (!/^\d{6}$/.test(this.smsCode)) {
+        this.$toast('请输入正确的手机验证码')
+        return
+      }
+      await condeLogin(this.smsCode, this.mobile)
+      Toast('登录成功')
+      this.$router.push('/')
+    },
+    async getCode() {
+      if (!this.validFn()) {
+        return
+      }
+      if (!this.timer && this.second === this.totalSecond) {
+        // 发送请求
+        const res = await getMessageCode(this.picCode, this.pickey, this.mobile)
+        console.log(res)
+        Toast('验证码发送成功')
+        this.timer = setInterval(() => {
+          this.second--
+          if (this.second <= 0) {
+            clearInterval(this.timer)
+            this.timer = null
+            this.second = this.totalSecond
+          }
+        }, 1000)
+      }
     }
+  },
+  destroyed() {
+    // 离开页面，清除定时器
+    clearInterval(this.timer)
   }
 }
 </script>
